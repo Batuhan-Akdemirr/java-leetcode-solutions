5ab99767b0c07b3d361a95e50d7da29522ce3408
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2023-09-06 20:59:06 +0300

    Power of Two solved

---
+++ leetcode/src/PowerOfTwo.java
@@ -0,0 +1,26 @@
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        System.out.println(PowerOfTwo.isPowerOfTwo(122));
+    }
+
+    private static boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        if (n % 2 != 0) {
+            return false;
+        }
+        return isPowerOfTwo(n / 2);
+
+    }
+
+}
+
+
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+* */
