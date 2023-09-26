848242a526463f47f771acc7b51ef3e5564d6aad
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2023-09-26 11:34:57 +0300

    1108. Defanging an IP Address solved

---
+++ leetcode/src/DefanginganIPAddress.java
@@ -0,0 +1,24 @@
+public class DefanginganIPAddress {
+    public static void main(String[] args) {
+        System.out.println(DefanginganIPAddress.defangIPaddr("1.1.1.1"));
+    }
+
+    private static String defangIPaddr(String address) {
+        return address.replace("." , "[.]");
+    }
+}
+
+/*
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+ */
