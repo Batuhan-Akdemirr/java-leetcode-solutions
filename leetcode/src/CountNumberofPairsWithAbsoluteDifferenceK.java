749611401f7c23cce89e8ba65e0bddb5c0b32caa
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2024-01-04 00:03:23 +0300

    2006. Count Number of Pairs With Absolute Difference K solved

---
+++ leetcode/src/CountNumberofPairsWithAbsoluteDifferenceK.java
@@ -0,0 +1,54 @@
+public class CountNumberofPairsWithAbsoluteDifferenceK {
+    public static void main(String[] args) {
+        int [] arr = {1,2,2,1};
+        System.out.println(CountNumberofPairsWithAbsoluteDifferenceK.countKDifference(arr,1));
+    }
+
+    private static int countKDifference(int[] nums, int k) {
+
+        int counter =0;
+        for(int i=0; i<nums.length-1; i++) {
+            for(int j= i+1; j<nums.length; j++) {
+                if(Math.abs(nums[i] - nums [j]) == k) {
+                    counter++;
+                }
+            }
+
+        }
+        return counter;
+    }
+}
+
+
+/*
+Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+The value of |x| is defined as:
+
+x if x >= 0.
+-x if x < 0.
+
+Example 1:
+
+Input: nums = [1,2,2,1], k = 1
+Output: 4
+Explanation: The pairs with an absolute difference of 1 are:
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1]
+Example 2:
+
+Input: nums = [1,3], k = 3
+Output: 0
+Explanation: There are no pairs with an absolute difference of 3.
+Example 3:
+
+Input: nums = [3,2,1,5,4], k = 2
+Output: 3
+Explanation: The pairs with an absolute difference of 2 are:
+- [3,2,1,5,4]
+- [3,2,1,5,4]
+- [3,2,1,5,4]
+
+ */
