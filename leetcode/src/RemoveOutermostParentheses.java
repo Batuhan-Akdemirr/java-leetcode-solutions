e8f7f1ae914cef099b56d63893c26b61924ba363
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2024-01-10 21:27:54 +0300

    1021. Remove Outermost Parentheses solved

---
+++ leetcode/src/RemoveOutermostParentheses.java
@@ -0,0 +1,63 @@
+public class RemoveOutermostParentheses {
+    public static void main(String[] args) {
+        System.out.println(RemoveOutermostParentheses.removeOuterParentheses("(()())(())"));
+    }
+
+    private static String removeOuterParentheses(String s) {
+        int count = 0;
+        StringBuilder result = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (count != 0) {
+                    result.append(c);
+                }
+                count++;
+            } else {
+                if (count != 1) {
+                    result.append(c);
+                }
+                count--;
+            }
+        }
+
+        return result.toString();
+    }
+}
+
+
+/*
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+
+
+Example 1:
+
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation:
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation:
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: s = "()()"
+Output: ""
+Explanation:
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+
+ */
