29a4b4657cb31652854814272f21d2912dbec724
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2024-02-04 14:34:37 +0300

    118. Pascal's Triangle solved

---
+++ leetcode/src/PascalsTriangle.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        System.out.println(PascalsTriangle.generate(5));
+    }
+
+    private static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int val = ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j);
+                    row.add(val);
+                }
+            }
+            ans.add(row);
+        }
+        return ans;
+    }
+}
+
+/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+ */
